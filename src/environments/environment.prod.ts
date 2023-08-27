d0ff295651adfd2efbc6b638e56884b7a1143679
3bdee <abdeeed2002@gmail.com>
2023-08-27 20:10:37 +0100

    okkk

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,12 @@
+import '../../.env';
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: 'AIzaSyCYUTBVy7D3GdHJ08hX0d_IG2EUsTHziao',
-    authDomain: 'mini-projet-angular-2b135.firebaseapp.com',
-    projectId: 'mini-projet-angular-2b135',
-    storageBucket: 'mini-projet-angular-2b135.appspot.com',
-    messagingSenderId: '809878661017',
-    appId: '1:809878661017:web:21da4ecd54351913c2d508',
+    apiKey: API_KEY,
+    authDomain: AUTH_DOMAIN,
+    projectId: PROJECT_ID,
+    storageBucket: STORAGE_BUCKET,
+    messagingSenderId: MESSAGING_SENDER_ID,
+    appId: APP_ID,
   },
 };
